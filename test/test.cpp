1870135592e7bc26b61626b0e1345d40eba376d2
kyy08 <kyyelzz1@gmail.com>
2023-05-18 22:25:57 +0700

    membuat case 1

--- test/test.cpp
+++ test/test.cpp
@@ -146,4 +146,13 @@ int main() {
 			cin >> pilihan;
 			switch (pilihan)
 			{
+			case 1:
+				addNode();
+				cout << "Data Berhasil Ditambahkan" << endl;
+				system("pause");
+				system("cls");
+				break;
+
+
+
 
